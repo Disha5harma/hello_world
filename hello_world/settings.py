06e6d0e064ba2416a608b1fd9ac0d18c444dbdd2
Disha Sharma <sharma.disha234@gmail.com>
2020-03-20 13:23:37 +0530

    made changes

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello_world',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,13 @@ WSGI_APPLICATION = 'hello_world.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hello_world',
-        'USER': 'hello_world',
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("172.30.200.191"),
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "helloworldapp",
+        "USER": "helloworldapp",
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": "",
     }
 }
 
